b5099aed9263a8d8bc38ea8d0e86ff93b0c82011
Jom Dollesin <gitnightjd@gmail.com>
2022-04-06 09:48:06 +0800

    New config for emacs update.

--- init.el
+++ init.el
@@ -470,92 +470,91 @@ folder, otherwise delete a word"
 (setq-default fill-column 80)
 
 (use-package simple-httpd
-  :ensure t)
+	:ensure t)
 
 (defun jd/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (auto-fill-mode 0)
-  (visual-line-mode 1)
-  (setq evil-auto-indent nil)
-  (diminish org-indent-mode))
+	(org-indent-mode)
+	(variable-pitch-mode 1)
+	(auto-fill-mode 0)
+	(visual-line-mode 1)
+	(setq evil-auto-indent nil)
+	(diminish org-indent-mode))
 
 (use-package org
-  :defer t
-  :hook (org-mode . jd/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▼ "
-        org-hide-emphasis-markers t
-        org-src-fontify-natively t
-        org-fontify-quote-and-verse-blocks t
-        org-src-tab-acts-natively t
-        org-edit-src-content-indentation 2
-        org-hide-block-startup nil
-        org-src-preserve-indentation nil
-        org-startup-folded 'content
-        org-cycle-separator-lines 2)
+	:defer t
+	:hook (org-mode . jd/org-mode-setup)
+	:config
+	(setq org-ellipsis " ▼ "
+				org-hide-emphasis-markers t
+				org-src-fontify-natively t
+				org-fontify-quote-and-verse-blocks t
+				org-src-tab-acts-natively t
+				org-edit-src-content-indentation 2
+				org-hide-block-startup nil
+				org-src-preserve-indentation nil
+				org-startup-folded 'content
+				org-cycle-separator-lines 2)
 
-  (setq org-modules
-        '(org-crypt
-          org-habit
-          org-bookmark
-          org-eshell
-          org-irc))
-  (setq org-refile-targets '((nil :maxlevel . 1)
-                             (org-agenda-files :maxlevel . 1)))
+	(setq org-modules
+				'(org-crypt
+					org-habit
+					org-bookmark
+					org-eshell
+					org-irc))
+	(setq org-refile-targets '((nil :maxlevel . 1)
+														 (org-agenda-files :maxlevel . 1)))
 
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-refiles-use-outline-path t)
+	(setq org-outline-path-complete-in-steps nil)
+	(setq org-refiles-use-outline-path t)
 
-  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-j") 'org-next-visible-heading)
-  (evil-define-key '(normal insert visual) org-mode-map (kbd "C-k") 'org-previous-visible-heading)
+	(evil-define-key '(normal insert visual) org-mode-map (kbd "C-j") 'org-next-visible-heading)
+	(evil-define-key '(normal insert visual) org-mode-map (kbd "C-k") 'org-previous-visible-heading)
 
-  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-j") 'org-metadown)
-  (evil-define-key '(normal insert visual) org-mode-map (kbd "M-k") 'org-metaup)
+	(evil-define-key '(normal insert visual) org-mode-map (kbd "M-j") 'org-metadown)
+	(evil-define-key '(normal insert visual) org-mode-map (kbd "M-k") 'org-metaup)
 
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((emacs-lisp . t)
-     (ledger . t)))
+	(org-babel-do-load-languages
+	 'org-babel-load-languages
+	 '((emacs-lisp . t)))
 
-  (push '("conf-unix" . conf-unix) org-src-lang-modes)
+	(push '("conf-unix" . conf-unix) org-src-lang-modes)
 
-  (use-package org-superstar
-    :after org
-    :hook (org-mode . org-superstar-mode)
-    :custom
-    (org-directory "~/Docs/notes/")
-    (org-superstar-remove-leading-stars t)
-    (org-ellipsis " ▼")
-    (org-superstar-item-bullet-alist '((?+ . ?) (?- . ?✦)))
-    (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
+	(use-package org-superstar
+		:after org
+		:hook (org-mode . org-superstar-mode)
+		:custom
+		(org-directory "~/Docs/notes/")
+		(org-superstar-remove-leading-stars t)
+		(org-ellipsis " ▼")
+		(org-superstar-item-bullet-alist '((?+ . ?) (?- . ?✦)))
+		(org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-  ;; Increase the size of various headings
-  (set-face-attribute 'org-document-title nil :font "VictorMono Nerd Font" :weight 'bold :height 1.3)
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "VictorMono Nerd Font" :weight 'medium :height (cdr face)))
+	;; Increase the size of various headings
+	(set-face-attribute 'org-document-title nil :font "VictorMono Nerd Font" :weight 'bold :height 1.3)
+	(dolist (face '((org-level-1 . 1.2)
+									(org-level-2 . 1.1)
+									(org-level-3 . 1.05)
+									(org-level-4 . 1.0)
+									(org-level-5 . 1.1)
+									(org-level-6 . 1.1)
+									(org-level-7 . 1.1)
+									(org-level-8 . 1.1)))
+	(set-face-attribute (car face) nil :font "VictorMono Nerd Font" :weight 'medium :height (cdr face)))
 
-  (require 'org-indent)
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+	(require 'org-indent)
+	(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+	(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+	(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+	(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+	(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+	(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+	(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
 
-  ;; Get rid of the background on column views
-  (set-face-attribute 'org-column nil :background nil)
-  (set-face-attribute 'org-column-title nil :background nil))
+	;; Get rid of the background on column views
+	(set-face-attribute 'org-column nil :background nil)
+	(set-face-attribute 'org-column-title nil :background nil))
 
 (require 'org-tempo)
 
